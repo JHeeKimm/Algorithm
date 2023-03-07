e275f2cd2cece9f7c9ea9f06dc81be945be1b571
JHeeKimm <94270128+JHeeKimm@users.noreply.github.com>
2023-03-07 13:46:50 +0900

    [Bronze V] Title: A+B - 5, Time: 136 ms, Memory: 9568 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.js
@@ -1,6 +1,6 @@
-const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
+const input = require('fs').readFileSync('/dev/stdin').toString().trim().split('\n');
 
-for(let i=0; i<input.length-2; i++){
-    let nums = input[i].split(' ').map(Number);
-    console.log(nums[0]+nums[1]);
+for(let i=0; i<input.length-1; i++){
+    let numbers = input[i].split(' ').map(Number);
+    console.log(numbers[0] + numbers[1]);
 }
