f901616377c1ec4b3a094abba8fdc8dedaa76b0d
JHeeKimm <94270128+JHeeKimm@users.noreply.github.com>
2023-03-20 14:59:45 +0900

    [Bronze II] Title: 바구니 순서 바꾸기, Time: 136 ms, Memory: 9468 KB -BaekjoonHub

--- 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.js
+++ 백준/Bronze/10812. 바구니 순서 바꾸기/바구니 순서 바꾸기.js
@@ -1,12 +1,12 @@
 const input = require('fs').readFileSync('/dev/stdin').toString().split('\n');
-const [N, M] = input[0].split(' ').map(Number);
+const [N, M] = input[0].split(' ');
 let arr = Array.from({length : N}, (v, index) => index + 1);
 
 for(let i=1; i<=M; i++){
-    const [a, b, c] = input[i].split(' ').map(Number);
-    let newArr = [];
-    newArr.push(arr.slice(c-1, b));
-    newArr.push(arr.slice(a-1, c-1));
-    arr.splice(a-1, b-a+1, ...newArr.flat());
+    const [a, b, c] = input[i].split(' ');
+    let newArr1 = arr.slice(c-1, b);
+    let newArr2 = arr.slice(a-1, c-1);
+    let newArr3 = newArr1.concat(newArr2)
+    arr.splice(a-1, b-a+1, ...newArr3).flat();
 }
-console.log(arr.flat().join(' '));
+console.log(arr.join(' '));
