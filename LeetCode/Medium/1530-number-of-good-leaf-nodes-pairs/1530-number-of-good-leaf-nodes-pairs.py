4fe199d94a2d1d360a472fae815da1f37df0b71c
JHeeKimm <94270128+JHeeKimm@users.noreply.github.com>
2024-08-23 15:33:04 +0900

    Time: 79 ms (86.51%), Space: 16.8 MB (95.14%) - LeetHub

---
+++ LeetCode/Medium/1530-number-of-good-leaf-nodes-pairs/1530-number-of-good-leaf-nodes-pairs.py
@@ -0,0 +1,40 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def countPairs(self, root: TreeNode, distance: int) -> int:
+        self.result = 0
+        
+        def dfs(node):
+            if not node:
+                return []
+            
+            if not node.left and not node.right:
+                return [1]
+
+            left = dfs(node.left)
+            right = dfs(node.right)
+            
+            for l in left:
+                for r in right:
+                    if l + r <= distance:
+                        self.result += 1
+                        
+            new_distances = []
+
+            for x in left:
+                if x+1 <= distance:
+                    new_distances.append(x+1)
+     
+            for x in right:
+                if x+1 <= distance:
+                    new_distances.append(x+1)
+                    
+            return new_distances
+        
+        dfs(root)
+        
+        return self.result
