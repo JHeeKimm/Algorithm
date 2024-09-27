9b0ba2f6fa034f8ce3f6b619533405d498f48348
JHeeKimm <94270128+JHeeKimm@users.noreply.github.com>
2024-09-27 18:38:17 +0900

    [Bronze I] Title: 전주 듣고 노래 맞히기, Time: 212 ms, Memory: 111384 KB -BaekjoonHub

--- 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.py
+++ 백준/Bronze/31562. 전주 듣고 노래 맞히기/전주 듣고 노래 맞히기.py
@@ -1,8 +1,7 @@
 # 1. 입력
-#   - 첫째 줄: 정환이 음을 아는 노래의 개수 N, 
+#   - 첫째 줄: 정환이 음을 아는 노래의 개수 N과
 #             정환이 맞히기를 시도할 노래의 개수 M이 공백으로 구분되어 주어짐
-#   - 둘째 줄 ~ N개의 줄: 
-#       아는 노래 제목의 길이 T, 제목 S, 음이름이 공백으로 구분되어 주어짐
+#   - 둘째 줄 ~ N개의 줄: 아는 노래 제목의 길이 T, 제목 S, 음이름이 공백으로 구분되어 주어짐
 #   - N+2 줄 ~ M개의 줄: 맞히길 시도할 노래의 첫 세 음
 # 2. 출력
 #   - 동일한 노래가 1개면 해당 '노래 제목'을 출력
@@ -14,27 +13,28 @@
 #   -> 맞히기 시도 시 해당 음과 비교
 
 n, m = map(int, input().split())
-song = {}
+song = {} # 아는 노래 담을 딕셔너리
 
 for _ in range(n):
-    # 아는 노래 길이, 제목, 음이름 입력 받기
+    # 아는 노래의 제목 길이, 제목, 음이름 입력 받기
     t, s, a1, a2, a3, a4, a5, a6, a7 = input().split()
-    notes = [a1, a2, a3] # 첫 세 음만 리스트로 저장
-    song[s] = notes # key가 노래 제목, value가 첫 세 음
+    # 아는 노래 담기
+    song[s] = [a1, a2, a3] # key가 노래 제목, value가 첫 세 음
 
 for _ in range(m):
-    try_song = input().split()
+    try_song = input().split() # 시도할 노래 입력 받기
     count = 0
-    title =''
+    title = ''
 
+    # 아는 노래 딕셔너리에서 일치하는 것이 있다면 카운트 증가, 해당 제목으로 재할당
     for i in song:
         if song[i] == try_song:
             count += 1
             title = i
 
-    if count == 1:
+    if count == 1: # 일치하는 노래가 1개면 제목 출력
         print(title)
-    elif count >= 2:
+    elif count >= 2: # 일치하는 노래가 2개 이상이면 ? 출력
         print('?')
     else:
-        print('!')
+        print('!')  # 일치하는 노래가 없으면 ! 출력
