b8a833717877d80fccdf9125fe55877624261d38
JHeeKimm <94270128+JHeeKimm@users.noreply.github.com>
2024-09-28 00:08:45 +0900

    [Silver V] Title: 국회의원 선거, Time: 88 ms, Memory: 108080 KB -BaekjoonHub

--- 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
+++ 백준/Silver/1417. 국회의원 선거/국회의원 선거.py
@@ -1,23 +1,19 @@
 import heapq, sys
-input = sys.stdin.readline
+input = sys.stdin.readline # 수정1
 
 n = int(input())
 dasom = int(input()) # 다솜 (기호 1번)을 찍으려는 사람의 수
 
 hq = []
 
-for _ in range(1,n):
+for _ in range(1,n): # 수정2
     vote = int(input())
     heapq.heappush(hq, -vote) # 최대힙으로 하기 위해 '-' 붙임
 
 count = 0
-while hq:
+while hq and -hq[0]>=dasom:
     # 가장 많은 득표수 가져오기
     vote = -heapq.heappop(hq)
-
-    # 다솜이가 표가 많다면 종료
-    if vote < dasom:
-        break
 
     # 매수 -> 가장 많은 표 -1, 다솜 표 +1, 카운트 +1
     vote -= 1
