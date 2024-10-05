8065777734708ed7b1dd240dd35c1baf385315e9
JHeeKimm <94270128+JHeeKimm@users.noreply.github.com>
2024-10-05 15:54:02 +0900

    [Silver II] Title: 촌수계산, Time: 40 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/2644. 촌수계산/촌수계산.py
+++ 백준/Silver/2644. 촌수계산/촌수계산.py
@@ -1,18 +1,3 @@
-# 1. 입력
-#   - 첫째 줄: 전체 사람 수 n
-#   - 둘째 줄: 촌수 계산해야하는 두 사람의 번호
-#   - 셋째 줄: 부모 자식들 간의 관계 개수 m
-#   - 넷째 줄 ~ : x y (x는 y의 부모 번호)
-
-# 2. 출력
-#   - 두 사람의 촌수를 나타내는 정수 출력
-#   - 관계가 없으면 -1 출력
-
-# 3. 문제 해결
-#   - 부모-자식 1촌, 나와 할아버지 2촌, 나와 아버지 형제들 3촌 
-#   - visited -1해놓고 방문하면서 촌수 계산
-
-from collections import deque
 import sys
 input = sys.stdin.readline
 
@@ -30,18 +15,13 @@ for _ in range(m):
     graph[y].append(x)
 
 
-def bfs(a, b):
-    q = deque([a])
-    while q:
-        p = q.popleft()
-
-        if p == b:
-            return visited[b]
-        
-        for i in graph[p]:
-            if visited[i] == 0:
-                visited[i] = visited[p] + 1
-                q.append(i)
-    return -1
-
-print(bfs(a, b))
+def dfs(p):
+    for i in graph[p]:
+        if visited[i] == 0:
+            visited[i] = visited[p]+1
+            dfs(i)
+dfs(a)
+if visited[b] > 0:
+    print(visited[b])
+else:
+    print(-1)
